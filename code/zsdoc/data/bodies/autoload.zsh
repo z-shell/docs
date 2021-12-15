ae21f8e74a9283deb0f216ab300f060919b811f2
Salvydas Lukosius <sall@w-ss.io>
2021-12-15 23:30:57 +0000

    Cleanup

--- code/zsdoc/data/bodies/autoload.zsh
+++
@@ -1,4 +0,0 @@
-
-
-builtin source "${ZI[BIN_DIR]}/lib/zsh/side.zsh" || { builtin print -P "${ZI[col-error]}ERROR:%f%b Couldn't find ${ZI[col-obj]}/lib/zsh/side.zsh%f%b."; return 1; }
-ZI[EXTENDED_GLOB]=""
--- wiki/zi/_Footer.md
+++
@@ -1,1 +0,0 @@
-We are looking for co-maintainers and contributors.
